a726d49a4266d96ae814a8120952ae183000c499
moselle123 <95174028+moselle123@users.noreply.github.com>
2023-04-10 18:13:34 +0100

    new nav and mind maps almost done

--- Assistant.js
+++ Assistant.js
@@ -1,4 +1,4 @@
-function assistantQuestion(event){
+function assistantQuestionHome(event){
     event.preventDefault();
     let question = document.getElementById("assistantQ");
     let modalInput = document.getElementById("modalInput");
@@ -6,8 +6,29 @@ function assistantQuestion(event){
     questions(question.value);
 }
 
+function assistantQuestionModal(event){
+    event.preventDefault();
+    let modalInput = document.getElementById("modalInput");
+    questions(modalInput.value);
+}
+
 function questions(question){
     if (question == "When is my next UI lecture?"){
         document.getElementById("answer").innerHTML = "<p>Your next UI lecture is on Monday 17th of March 2023 at 13:00. Would you like to see the full event information?<p><button class=\"btn btn-primary m-2\">Yes</button><button class=\"btn btn-primary m-2\">No</button>";
     }
 }
+
+function clearQuestions(){
+    let question = document.getElementById("assistantQ");
+    let modalInput = document.getElementById("modalInput");
+    let answerDiv = document.getElementById("answer");
+    modalInput.value = "";
+    question.value = "";
+    answerDiv.innerHTML = "";
+}
+
+$(document).ready(function() {
+    $('#assistantModal').on('hidden.bs.modal', function (e) {
+      clearQuestions();
+    });
+  });
