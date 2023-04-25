29da4109d28aa468393c23c01dfb366737a25181
moselle123 <95174028+moselle123@users.noreply.github.com>
2023-04-25 13:20:00 +0100

    module pages done, settings almost finished

--- Grades.js
+++ Grades.js
@@ -6,14 +6,16 @@ var modulesList = JSON.parse(window.localStorage.getItem("modulesList")) || [
       credits: 15,
       average: 80.7,
       achieved: 56.5,
-      completed: 70
+      completed: 70,
+      url: "SoftwareArch.html"
     },
     {
-      name: "Databases",
+      name: "Scrum",
       credits: 15,
-      average: 80,
-      achieved: 80,
-      completed: 100
+      average: 77,
+      achieved: 54,
+      completed: 70,
+      url: "Scrum.html"
     }
   ];
   //when the page loads add the divs from the information in the modules list
@@ -26,12 +28,12 @@ var modulesList = JSON.parse(window.localStorage.getItem("modulesList")) || [
         var completedWidth = modulesList[i].completed - modulesList[i].achieved;
         if (i % 2 === 0) {
             var html = "<div class=\"row mt-3\">";
-            html += "<div class=\"border rounded p-2 me-5\" style=\"width: 25vw;\"  onclick=\"clickModule(" + modulesList[i].name + ")\" id=\"moduleDiv\"><h6 class=\"text-center fw-bold\">" + modulesList[i].name + "</h6><div class=\"d-flex justify-content-evenly mt-3\"><div class=\"d-flex flex-column align-items-center\"><h6>" + modulesList[i].average + "%</h6><p>Average</p></div><div class=\"d-flex flex-column align-items-center text-success\"><h6>" + modulesList[i].achieved +"%</h6><p>Achieved</p></div><div class=\"d-flex flex-column align-items-center text-danger\"><h6>" + modulesList[i].completed +"%</h6><p>Complete</p></div></div><div class=\"progress-stacked mx-3\"><div class=\"progress\" role=\"progressbar\" aria-label=\"Achieved\" aria-valuenow=\"" + modulesList[i].achieved +"\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + modulesList[i].achieved + "%;\"><div class=\"progress-bar bg-success\"></div></div><div class=\"progress\" role=\"progressbar\" aria-label=\"Complete\" aria-valuenow=\"" + modulesList[i].completed + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + completedWidth +"%;\"><div class=\"progress-bar bg-danger\"></div></div></div></div>";
+            html += "<div class=\"border rounded p-2 me-5\" style=\"width: 25vw;\"  onclick=\"clickModule("+i+")\"><h6 class=\"text-center fw-bold\">" + modulesList[i].name + "</h6><div class=\"d-flex justify-content-evenly mt-3\"><div class=\"d-flex flex-column align-items-center\"><h6>" + modulesList[i].average + "%</h6><p>Average</p></div><div class=\"d-flex flex-column align-items-center text-success\"><h6>" + modulesList[i].achieved +"%</h6><p>Achieved</p></div><div class=\"d-flex flex-column align-items-center text-danger\"><h6>" + modulesList[i].completed +"%</h6><p>Complete</p></div></div><div class=\"progress-stacked mx-3\"><div class=\"progress\" role=\"progressbar\" aria-label=\"Achieved\" aria-valuenow=\"" + modulesList[i].achieved +"\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + modulesList[i].achieved + "%;\"><div class=\"progress-bar bg-success\"></div></div><div class=\"progress\" role=\"progressbar\" aria-label=\"Complete\" aria-valuenow=\"" + modulesList[i].completed + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + completedWidth +"%;\"><div class=\"progress-bar bg-danger\"></div></div></div></div>";
             if (i === modulesList.length - 1) {
                 html += "</div>"; // Close the row if this is the last module
             }
         } else {
-            var html = "<div class=\" border rounded p-2 me-5\" style=\"width: 25vw;\"  onclick=\"clickModule(" + modulesList[i].name + ")\" id=\"moduleDiv\"><h6 class=\"text-center fw-bold\"><div>" + modulesList[i].name + "</h6><div class=\"d-flex justify-content-evenly mt-3\"><div class=\"d-flex flex-column align-items-center\"><h6>" + modulesList[i].average + "%</h6><p>Average</p></div><div class=\"d-flex flex-column align-items-center text-success\"><h6>" + modulesList[i].achieved +"%</h6><p>Achieved</p></div><div class=\"d-flex flex-column align-items-center text-danger\"><h6>" + modulesList[i].completed +"%</h6><p>Complete</p></div></div><div class=\"progress-stacked mx-3\"><div class=\"progress\" role=\"progressbar\" aria-label=\"Achieved\" aria-valuenow=\"" + modulesList[i].achieved +"\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + modulesList[i].achieved + "%;\"><div class=\"progress-bar bg-success\"></div></div><div class=\"progress\" role=\"progressbar\" aria-label=\"Complete\" aria-valuenow=\"" + modulesList[i].completed + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + completedWidth +"%;\"><div class=\"progress-bar bg-danger\"></div></div></div></div></div>";
+            var html = "<div class=\" border rounded p-2 me-5\" style=\"width: 25vw;\"  onclick=\"clickModule("+i+")\"><h6 class=\"text-center fw-bold\"><div>" + modulesList[i].name + "</h6><div class=\"d-flex justify-content-evenly mt-3\"><div class=\"d-flex flex-column align-items-center\"><h6>" + modulesList[i].average + "%</h6><p>Average</p></div><div class=\"d-flex flex-column align-items-center text-success\"><h6>" + modulesList[i].achieved +"%</h6><p>Achieved</p></div><div class=\"d-flex flex-column align-items-center text-danger\"><h6>" + modulesList[i].completed +"%</h6><p>Complete</p></div></div><div class=\"progress-stacked mx-3\"><div class=\"progress\" role=\"progressbar\" aria-label=\"Achieved\" aria-valuenow=\"" + modulesList[i].achieved +"\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + modulesList[i].achieved + "%;\"><div class=\"progress-bar bg-success\"></div></div><div class=\"progress\" role=\"progressbar\" aria-label=\"Complete\" aria-valuenow=\"" + modulesList[i].completed + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + completedWidth +"%;\"><div class=\"progress-bar bg-danger\"></div></div></div></div></div>";
         }
         fullHTML += html;
     }
@@ -68,6 +70,8 @@ function newModule(){
     });
 }
 
-function clickModule(moduleName){
-
-}
+function clickModule(index) {
+    if (index === 0 || index === 1){
+        window.location.href = modulesList[index].url;
+    }
+  }
